ccc7c526a115b5788c114bd5c102d30d8edfaa0f
apoplawski96 <a.poplawski96@gmail.com>
2021-08-18 21:44:20 +0200

    Create GuestScreen, extract HelpsTextField composable

--- app/src/main/java/com/helps/presentation/common/composable/HelpsTextComposables.kt
+++ app/src/main/java/com/helps/presentation/common/composable/HelpsTextComposables.kt
@@ -1,8 +1,12 @@
 package com.helps.presentation.common.composable
 
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -10,6 +14,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.helps.presentation.common.theme.HelpsThemeWhite
 
 @Composable
 fun HelpsMottoText(text: String) {
@@ -23,4 +28,32 @@ fun HelpsMottoText(text: String) {
             color = Color.White,
         ),
     )
+}
+
+@Composable
+fun HelpsTextField(usernameText: String, label: String, onTextChanged: (String) -> Unit) {
+    Column(
+        modifier = Modifier.padding(vertical = 24.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = usernameText,
+            onValueChange = onTextChanged,
+            label = { Text(label) },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = HelpsThemeWhite,
+                unfocusedBorderColor = HelpsThemeWhite,
+                focusedLabelColor = HelpsThemeWhite,
+                unfocusedLabelColor = HelpsThemeWhite
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(0.5.dp)
+                .background(HelpsThemeWhite)
+        )
+    }
 }
